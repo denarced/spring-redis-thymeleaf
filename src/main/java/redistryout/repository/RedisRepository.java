ec3e79ca24ace974669e8f126066fdabe0c9761f
denarced <denarced@gmail.com>
2015-06-13 18:39:31 +0300

    Comment code, tweak bootstrap layout.

--- src/main/java/redistryout/repository/RedisRepository.java
+++ src/main/java/redistryout/repository/RedisRepository.java
@@ -4,8 +4,27 @@ import java.util.Map;
 import java.util.Set;
 
 public interface RedisRepository {
+    /**
+     * Return a set of keys in Redis: all of them.
+     *
+     * @return non null.
+     */
     Set<String> keys();
+
+    /**
+     * Return key-value pairs in Redis: all of them.
+     *
+     * @return non null.
+     */
     Map<String,String> values();
+
+    /**
+     * Add key-value pair to Redis.
+     */
     void add(String key, String value);
+
+    /**
+     * Delete a key-value pair in Redis.
+     */
     void delete(String key);
 }
--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,11 +1,26 @@
+/**
+ * Create HTML table row.
+ *
+ * \param text (str) HTML code to be placed inside the row.
+ */
 function tr(text) {
     return '<tr>' + text + '</tr>';
 }
 
+/**
+ * Create HTML table cell element.
+ *
+ * \param text (str) The text to be placed inside the cell.
+ */
 function td(text) {
     return '<td>' + text + '</td>';
 }
 
+/**
+ * Create HTML button with which a key-value pair is deleted from persistence.
+ *
+ * \param key (str) that'll be deleted when the created button is clicked.
+ */
 function deleteButton(key) {
     var format = '<button '
         + 'class="btn btn-default" '
@@ -15,10 +30,19 @@ function deleteButton(key) {
     return format.replace(/{key}/g, key);
 }
 
+/**
+ * Create HTML table row element.
+ *
+ * \param key (str) text into the key cell.
+ * \param value (str) text into the value cell.
+ */
 function row(key, value) {
     return $(tr(td(key) + td(value) + td(deleteButton(key))));
 }
 
+/**
+ * Clear and reload the values in data table.
+ */
 function refreshTable() {
     $.get('/values', function(data) {
         var attr,
@@ -32,7 +56,17 @@ function refreshTable() {
     });
 }
 
+/**
+ * Delete key-value pair.
+ *
+ * \param key (str) The key to be deleted.
+ */
 function deleteKey(key) {
+    /*
+     * Delete the key.
+     * Reload keys and values in table to reflect the deleted ones.
+     * Set keyboard focus to key input: ready to start typing.
+     */
     $.post('/delete', {key: key}, function() {
         refreshTable();
         $('#keyInput').focus();
@@ -49,14 +83,21 @@ $(document).ready(function() {
             key: keyInput.val(),
             value: valueInput.val()
         };
+        /*
+         * Persist the new key-value pair.
+         * Clear the inputs.
+         * Set keyboard focus to key input: ready to start typing.
+         */
         $.post('/add', data, function() {
             refreshTable();
             keyInput.val('');
             valueInput.val('');
             keyInput.focus();
         });
+        /* Prevent HTTP form submit */
         event.preventDefault();
     });
 
+    /* Focus keyboard input into key input; ready to start typing */
     keyInput.focus();
 });
